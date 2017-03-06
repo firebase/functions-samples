f6a8f76b78429811ebc8cb3b060cae4db10e0e7a
Nicolas Garnier <nivco@google.com>
2017-03-05 18:44:02 -0800

    refactored moderate-image for clearer less deep Promise imbractions
    Change-Id: Ifb432f54729913774478478dd4b7f5cb7c1f44e5

--- moderate-images/functions/index.js
+++ moderate-images/functions/index.js
@@ -55,26 +55,27 @@ function blurImage(filePath, bucketName, metadata) {
   const fileDir = filePathSplit.join('/');
   const tempLocalDir = `${LOCAL_TMP_FOLDER}${fileDir}`;
   const tempLocalFile = `${LOCAL_TMP_FOLDER}${filePath}`;
+  const bucket = gcs.bucket(bucketName);
 
   // Create the temp directory where the storage file will be downloaded.
   return mkdirp(tempLocalDir).then(() => {
+    console.log('Temporary directory has been created', tempLocalDir);
     // Download file from bucket.
-    const bucket = gcs.bucket(bucketName);
     return bucket.file(filePath).download({
       destination: tempLocalFile
-    }).then(() => {
-      console.log('The file has been downloaded to', tempLocalFile);
-      // Blur the image using ImageMagick.
-      return exec(`convert ${tempLocalFile} -channel RGBA -blur 0x8 ${tempLocalFile}`).then(() => {
-        console.log('Blurred image created at', tempLocalFile);
-        // Uploading the Blurred image.
-        return bucket.upload(tempLocalFile, {
-          destination: filePath,
-          metadata: {metadata: metadata} // Keeping custom metadata.
-        }).then(() => {
-          console.log('Blurred image uploaded to Storage at', filePath);
-        });
-      });
     });
+  }).then(() => {
+    console.log('The file has been downloaded to', tempLocalFile);
+    // Blur the image using ImageMagick.
+    return exec(`convert ${tempLocalFile} -channel RGBA -blur 0x8 ${tempLocalFile}`);
+  }).then(() => {
+    console.log('Blurred image created at', tempLocalFile);
+    // Uploading the Blurred image.
+    return bucket.upload(tempLocalFile, {
+      destination: filePath,
+      metadata: {metadata: metadata} // Keeping custom metadata.
+    });
+  }).then(() => {
+    console.log('Blurred image uploaded to Storage at', filePath);
   });
 }
