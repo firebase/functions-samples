cc2f3e5004a3424f277569650be1af86d0972488
James Daniels <jamesdaniels@google.com>
2017-03-07 12:29:45 -0800

    Adding error handling to Stripe examples charge customer
    Change-Id: I077a3aabc74bd1e44301ada7e41e18d6e344b0d0

--- stripe/functions/index.js
+++ stripe/functions/index.js
@@ -1,11 +1,30 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
 const functions = require('firebase-functions'),
-      admin = require('firebase-admin');
+      admin = require('firebase-admin'),
+      logging = require('@google-cloud/logging')();
 
 admin.initializeApp(functions.config().firebase);
 
 const stripe = require('stripe')(functions.config().stripe.token),
       currency = functions.config().stripe.currency || 'USD';
 
+// [START chargecustomer]
 exports.createStripeCharge = functions.database.ref('/users/{userId}/charges/{id}').onWrite(event => {
   const val = event.data.val();
   if (val === null || val.id || val.error) return null;
@@ -18,10 +37,15 @@ exports.createStripeCharge = functions.database.ref('/users/{userId}/charges/{id
   }).then(response => {
       return event.data.ref.set(response);
     }, error => {
-      return event.data.ref.child('error').set(error.message);
+      // We want to capture errors and render them in a user-friendly way, while
+      // still logging an exception with Stackdriver
+      return event.data.ref.child('error').set(userFacingMessage(error)).then(() => {
+        return reportError(error, {user: event.params.userId});
+      });
     }
   );
 });
+// [END chargecustomer]]
 
 exports.createStripeCustomer = functions.auth.user().onCreate(event => {
   const data = event.data;
@@ -41,3 +65,45 @@ exports.cleanupUser = functions.auth.user().onDelete(event => {
     return admin.database().ref(`/stripe_customers/${event.data.uid}`).remove();
   });
 });
+
+// [START reporterror]
+function reportError(err, context = {}) {
+  // This is the name of the StackDriver log stream that will receive the log
+  // entry. This name can be any valid log stream name, but must contain "err"
+  // in order for the error to be picked up by StackDriver Error Reporting.
+  const logName = 'errors';
+  const log = logging.log(logName);
+
+  const metadata = {
+    // https://cloud.google.com/logging/docs/api/ref_v2beta1/rest/v2beta1/MonitoredResource
+    resource: {
+      type: 'cloud_function',
+      labels: { function_name: process.env.FUNCTION_NAME }
+    }
+  };
+
+  // https://cloud.google.com/error-reporting/reference/rest/v1beta1/ErrorEvent
+  const errorEvent = {
+    message: err.stack,
+    serviceContext: {
+      service: process.env.FUNCTION_NAME,
+      resourceType: 'cloud_function'
+    },
+    context: context
+  };
+
+  // Write the error log entry
+  return new Promise((resolve, reject) => {
+    log.write(log.entry(metadata, errorEvent), error => {
+      if (error) { reject(error); }
+      resolve();
+    });
+  });
+}
+// [END reporterror]
+
+function userFacingMessage(error) {
+  return error.type ? error.message : 'An error occurred, developers have been alerted';
+}
+
+
