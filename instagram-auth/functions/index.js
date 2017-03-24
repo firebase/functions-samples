59a95f95cc2b4901dcf34bfd68e5461f149039a8
Olivier Haouchine <haouchineo@gmail.com>
2017-03-24 16:49:08 +0100

    createCustomToken returns a promise

--- instagram-auth/functions/index.js
+++ instagram-auth/functions/index.js
@@ -147,8 +147,9 @@ function createFirebaseAccount(instagramID, displayName, photoURL, accessToken) 
   // Wait for all async task to complete then generate and return a custom auth token.
   return Promise.all([userCreationTask, databaseTask]).then(() => {
     // Create a Firebase custom auth token.
-    const token = admin.auth().createCustomToken(uid);
-    console.log('Created Custom token for UID "', uid, '" Token:', token);
-    return token;
+    return admin.auth().createCustomToken(uid).then(function (token) {
+      console.log('Created Custom token for UID "', uid, '" Token:', token);
+      return token;
+    });
   });
 }
