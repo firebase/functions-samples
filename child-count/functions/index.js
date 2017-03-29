15ea32906a3831e18597ccc1d0418fca39584292
katowulf <katowulf@google.com>
2017-03-29 14:00:45 -0700

    More es6 cleanup (var to const)

--- child-count/functions/index.js
+++ child-count/functions/index.js
@@ -21,8 +21,8 @@ admin.initializeApp(functions.config().firebase);
 
 // Keeps track of the length of the 'likes' child list in a separate property.
 exports.countlikechange = functions.database.ref('/posts/{postid}/likes/{likeid}').onWrite(event => {
-  var collectionRef = event.data.ref.parent;
-  var countRef = collectionRef.parent.child('likes_count');
+  const collectionRef = event.data.ref.parent;
+  const countRef = collectionRef.parent.child('likes_count');
 
   // return the promise from .transaction() so our function 
   // waits for this async event to complete before it exits
@@ -39,8 +39,9 @@ exports.countlikechange = functions.database.ref('/posts/{postid}/likes/{likeid}
 // If the number of likes gets deleted, recount the number of likes
 exports.recountlikes = functions.database.ref('/posts/{postid}/likes_count').onWrite(event => {
   if (!event.data.exists()) {
-    var counterRef = event.data.ref;
-    var collectionRef = counterRef.parent.child('likes');
+    const counterRef = event.data.ref;
+    const collectionRef = counterRef.parent.child('likes');
+    
     // return the promise from counterRef.set() so our function 
     // waits for this async event to complete before it exits
     return collectionRef.once('value')
