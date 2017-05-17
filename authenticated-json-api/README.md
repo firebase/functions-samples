5715f8c53c1d5d01887b8a0147e8a7dc11eff8ec
Dana Silver <dsilver1221@gmail.com>
2017-05-16 17:13:07 -0700

    Add authenticated-json-api sample. (#3)

---
+++ authenticated-json-api/README.md
@@ -0,0 +1,40 @@
+# Authenticated JSON API
+
+This sample shows how to authenticate access to a JSON API to only allow
+access to data for a specific Firebase user.
+
+Only users who pass a valid Firebase ID token as a Bearer token in the
+`Authorization` header of the HTTP request are authorized to use the API.
+
+This sample comes with a web-based API explorer UI whose code is in the [public](public) directory.
+It lets you sign in to Firebase with your Google account, and create messages whose sentiments are
+detected by the [Cloud Natural Language API](https://cloud.google.com/natural-language/).
+
+## Setting up the sample
+
+ 1. Create a Firebase Project using the [Firebase Console](https://console.firebase.google.com).
+ 1. Enable the **Google** Provider in the **Auth** section.
+ 1. Enable Billing on your project (to connect to the Natural Language API) by switching to the Blaze or Flame plan.
+ 1. Clone or download this repo and open the `authenticated-json-api` directory.
+ 1. You must have the Firebase CLI installed. If you don't have it install it with `npm install -g firebase-tools` and then configure it with `firebase login`.
+ 1. Configure the CLI locally by using `firebase use --add` and select your project in the list.
+ 1. Install dependencies locally by running: `cd functions; npm install; cd -`
+ 1. Enable the Google Cloud Natural Language API: https://console.cloud.google.com/apis/api/language.googleapis.com/overview?project=_
+
+## Deploy and test
+
+This sample comes with a web-based UI for testing the function. To test it out:
+
+ 1. Deploy your project using `firebase deploy`
+ 1. Open the app using `firebase open hosting:site`, this will open a browser.
+ 1. Sign in to the web app in the browser using Google Sign-In
+ 1. Create messages and explore them using the List and Detail sections.
+ 1. Sign out. You should no longer be able to access the API.
+
+## Contributing
+
+We'd love that you contribute to the project. Before doing so please read our [Contributor guide](../CONTRIBUTING.md).
+
+## License
+
+© Google, 2017. Licensed under an [Apache-2](../LICENSE) license.
---
+++ authenticated-json-api/public/main.js
@@ -0,0 +1,150 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+function Demo() {
+  $(function() {
+    this.$signInButton = $('#demo-sign-in-button');
+    this.$signOutButton = $('#demo-sign-out-button');
+    this.$messageTextarea = $('#demo-message');
+    this.$createMessageButton = $('#demo-create-message');
+    this.$createMessageResult = $('#demo-create-message-result');
+    this.$messageListButtons = $('.message-list-button');
+    this.$messageList = $('#demo-message-list');
+    this.$messageDetails = $('#demo-message-details');
+
+    this.$signInButton.on('click', this.signIn.bind(this));
+    this.$signOutButton.on('click', this.signOut.bind(this));
+    this.$createMessageButton.on('click', this.createMessage.bind(this));
+    this.$messageListButtons.on('click', this.listMessages.bind(this));
+    firebase.auth().onAuthStateChanged(this.onAuthStateChanged.bind(this));
+  }.bind(this));
+}
+
+Demo.prototype.signIn = function() {
+  firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider());
+};
+
+Demo.prototype.signOut = function() {
+  firebase.auth().signOut();
+};
+
+Demo.prototype.onAuthStateChanged = function(user) {
+  if (user) {
+    // If we have a user, simulate a click to get all their messages.
+    // Material Design Lite will create a <span> child that we'll expect to be clicked
+    $('#message-list-button-all > span').click();
+    this.$messageTextarea.removeAttr('disabled');
+    this.$createMessageButton.removeAttr('disabled');
+  } else {
+    this.$messageTextarea.attr('disabled', true);
+    this.$createMessageButton.attr('disabled', true);
+    this.$createMessageResult.html('');
+    this.$messageList.html('');
+    this.$messageDetails.html('');
+  }
+};
+
+Demo.prototype.createMessage = function() {
+  var message = this.$messageTextarea.val();
+
+  if (message === '') return;
+
+  // Make an authenticated POST request to create a new message
+  this.authenticatedRequest('POST', '/api/messages', {message: message}).then(function(response) {
+    this.$messageTextarea.val('');
+    this.$messageTextarea.parent().removeClass('is-dirty');
+
+    this.$createMessageResult.html('Created <b>' + response.category + '</b> message: ' + response.message);
+  }.bind(this)).catch(function(error) {
+    console.log('Error creating message:', message);
+    throw error;
+  });
+};
+
+Demo.prototype.listMessages = function(event) {
+  this.$messageListButtons.removeClass('mdl-button--accent');
+  $(event.target).parent().addClass('mdl-button--accent');
+  this.$messageList.html('');
+  this.$messageDetails.html('');
+
+  // Make an authenticated GET request for a list of messages
+  // Optionally specifying a category (positive, negative, neutral)
+  var label = $(event.target).parent().text().toLowerCase();
+  var category = label === 'all' ? '' : label;
+  var url = category ? '/api/messages?category=' + category : '/api/messages';
+  this.authenticatedRequest('GET', url).then(function(response) {
+    var elements = response.map(function(message) {
+      return $('<li>')
+        .text(message.message)
+        .addClass('mdl-list__item')
+        .data('key', message.key)
+        .on('click', this.messageDetails.bind(this));
+    }.bind(this));
+
+    // Append items to the list and simulate a click to fetch the first message's details
+    this.$messageList.append(elements);
+
+    if (elements.length > 0) {
+      elements[0].click();
+    }
+  }.bind(this)).catch(function(error) {
+    console.log('Error listing messages.');
+    throw error;
+  });
+};
+
+Demo.prototype.messageDetails = function(event) {
+  $('li').removeClass('selected');
+  $(event.target).addClass('selected');
+
+  var key = $(event.target).data('key');
+  this.authenticatedRequest('GET', '/api/message/' + key).then(function(response) {
+    this.$messageDetails.text(JSON.stringify(response, null, 2));
+  }.bind(this)).catch(function(error) {
+    console.log('Error getting message details.');
+    throw error;
+  });
+};
+
+Demo.prototype.authenticatedRequest = function(method, url, body) {
+  if (!firebase.auth().currentUser) {
+    throw new Error('Not authenticated. Make sure you\'re signed in!');
+  }
+
+  // Get the Firebase auth token to authenticate the request
+  return firebase.auth().currentUser.getToken().then(function(token) {
+    var request = {
+      method: method,
+      url: url,
+      dataType: 'json',
+      beforeSend: function(xhr) { xhr.setRequestHeader('Authorization', 'Bearer ' + token); }
+    };
+
+    if (method === 'POST') {
+      request.contentType = 'application/json';
+      request.data = JSON.stringify(body);
+    }
+
+    console.log('Making authenticated request:', method, url);
+    return $.ajax(request).catch(function() {
+      throw new Error('Request error: ' + method + ' ' + url);
+    });
+  });
+};
+
+window.demo = new Demo();
