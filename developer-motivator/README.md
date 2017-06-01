f7ba1b8277b84066875e44814f0c20b4afa3b921
Emanuele Tido <tidoemanuele@gmail.com>
2017-06-01 20:24:38 +0100

    cleaning code

--- developer-motivator/README.md
+++ developer-motivator/README.md
@@ -15,3 +15,6 @@ The dependencies are listed in [functions/package.json](functions/package.json).
 ## Trigger rules
 
 The functions triggers every time a new user open your app the first time or remove your app from his device.
+
+## Setup and test this sample section
+Could you add a ## Setup and test this sample section giving a step by step process to follow in order to get the sample running, for instance the tricky part will be how to get a developer device token
--- developer-motivator/functions/index.js
+++ developer-motivator/functions/index.js
@@ -1,8 +1,29 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 'use strict';
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 admin.initializeApp(functions.config().firebase);
 
+/**
+ * Could you add some short comment explaining what the functions does?
+ * Endpoint which handles requests for a Google Assistant action which asks users to say a number
+ * and read out the ordinal of that number.
+ * e.g. If the user says "Twelve" the action will say "The ordinal of twelve is twelfth".
+ */
 exports.appinstalled = functions.analytics.event('first_open').onLog(event => {
     const payload = {
       notification: {
@@ -11,21 +32,15 @@ exports.appinstalled = functions.analytics.event('first_open').onLog(event => {
       }
     };
 
-     admin.messaging().sendToDevice("put_your_developer_device_token_here", payload).then(response => {
-      const tokensToRemove = [];
-      response.results.forEach((result, index) => {
-        const error = result.error;
-        if (error) {
-          console.error('Failure sending notification to', tokens[index], error);
-          if (error.code === 'messaging/invalid-registration-token' ||
-              error.code === 'messaging/registration-token-not-registered') {
-            tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
-          }
-        }
-      });
-    });
+     admin.messaging().sendToDevice("put_your_developer_device_token_here", payload);
 });
 
+/**
+ * Could you add some short comment explaining what the functions does? 
+ * Endpoint which handles requests for a Google Assistant action which asks users to say a number
+ * and read out the ordinal of that number.
+ * e.g. If the user says "Twelve" the action will say "The ordinal of twelve is twelfth".
+ */
 exports.appremoved = functions.analytics.event('app_remove').onLog(event => {
     const payload = {
       notification: {
@@ -34,19 +49,7 @@ exports.appremoved = functions.analytics.event('app_remove').onLog(event => {
       }
     };
 
-     admin.messaging().sendToDevice("put_your_developer_device_token_here", payload).then(response => {
-      const tokensToRemove = [];
-      response.results.forEach((result, index) => {
-        const error = result.error;
-        if (error) {
-          console.error('Failure sending notification to', tokens[index], error);
-          if (error.code === 'messaging/invalid-registration-token' ||
-              error.code === 'messaging/registration-token-not-registered') {
-            tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
-          }
-        }
-      });
-    });
+     admin.messaging().sendToDevice("put_your_developer_device_token_here", payload);
 
 });
 
