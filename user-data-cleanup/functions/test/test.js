5d42aad4b8885c5569a3ed7f21489fcde543ee81
Dan Zhang <dzdz@google.com>
2017-06-05 10:06:47 -0700

    code formatting changes

--- user-data-cleanup/functions/test/test.js
+++ user-data-cleanup/functions/test/test.js
@@ -20,70 +20,69 @@ const chaiAsPromised = require('chai-as-promised');
 const sinon = require('sinon');
 chai.use(chaiAsPromised);
 
-
-// test input data
+// test input data which will be shared accross test cases
 const userId = '8ZfiT8HeMTN9a4etjfCmahBqhK52';
 const displayName = 'My Name';
 
-//test cases
 describe('Wipeout', () => {
-	let  admin, functions, wipeout, configStub, databaseStub, refStub;
+  let  admin, functions, wipeout, configStub, databaseStub, refStub;
 
-	before(() => {
-		admin = require('firebase-admin');
-		adminInitStub = sinon.stub(admin,'initializeApp');
-		databaseStub = sinon.stub(admin, 'database');
-		refStub = sinon.stub();
-		databaseStub.returns( { ref: refStub });
+  before(() => {
+    // create database and configuration stubs
+    admin = require('firebase-admin');
+    adminInitStub = sinon.stub(admin,'initializeApp');
+    databaseStub = sinon.stub(admin, 'database');
+    refStub = sinon.stub();
+    databaseStub.returns( { ref: refStub });
 
-		functions = require('firebase-functions');
-		configStub = sinon.stub(functions, 'config').returns({
-			firebase:{
-				databaseURL:'https://fakedb.firebaseio.com',
-			},
-			wipeout:{
-					path:'/users'
-			}
-		});
+    functions = require('firebase-functions');
+    configStub = sinon.stub(functions, 'config').returns({
+      firebase:{
+        databaseURL:'https://fakedb.firebaseio.com',
+      },
+      wipeout:{
+          path:'/users'
+      }
+    });
 
-		wipeout = require('../wipeout');
+    wipeout = require('../wipeout');
 
-	});
+  });
 
-	after(() => {
-		configStub.restore();
-		adminInitStub.restore();
-	});
+  after(() => {
+    configStub.restore();
+    adminInitStub.restore();
+  });
 
-	describe('Delete User', () => {
-		const fakeUser = {
-			uid : userId,
-			displayName: displayName
-		};
+  describe('Delete User', () => {
+    const fakeUser = {
+      uid : userId,
+      displayName: displayName
+    };
 
-		it('should build correct path', () => {
-			expect(wipeout.buildPath(fakeUser.uid)).equal(`/users/${userId}`);
-			});
+    it('should build correct path', () => {
+      expect(wipeout.buildPath(fakeUser.uid)).equal(`/users/${userId}`);
+      });
 
-		it('should delete data', () => {
-			const removeParam = `/users/${userId}`;
-			const removeStub = sinon.stub();
-			refStub.withArgs(removeParam).returns( { remove : removeStub } );
-			removeStub.resolves("Removed");
+    it('should delete data', () => {
+      const removeParam = `/users/${userId}`;
+      const removeStub = sinon.stub();
+      refStub.withArgs(removeParam).returns( { remove : removeStub } );
+      removeStub.resolves("Removed");
 
-			return expect(wipeout.deleteUser(fakeUser)).to.eventually.equal("Removed");
-			});
+      return expect(wipeout.deleteUser(fakeUser)).to.eventually.equal("Removed");
+      });
 
-		it('should write log into logging path', () => {
-			const logParam = `/wipeout-log/${userId}`;
-			const setStub = sinon.stub();
-			refStub.withArgs(logParam).returns( { set: setStub} );
-			setStub.withArgs(displayName).resolves('Log added');
+    it('should write log into logging path', () => {
+      const logParam = `/wipeout-log/${userId}`;
+      const setStub = sinon.stub();
+      refStub.withArgs(logParam).returns( { set: setStub} );
+      setStub.withArgs(displayName).resolves('Log added');
 
-			return expect(wipeout.writeLog(fakeUser)).to.eventually.equal("Log added");
-			});
+      return expect(wipeout.writeLog(fakeUser)).to.eventually.equal("Log added");
+      });
 
-		});
+    });
 
-	});
+  });
 
