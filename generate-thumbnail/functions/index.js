ce138371d628c50ee11528f883d6c9ef8850379f
Jen Person <personjeh@gmail.com>
2017-06-12 17:12:44 -0700

    added to generate-thumbnail example to upload Signed URLs to the database

--- generate-thumbnail/functions/index.js
+++ generate-thumbnail/functions/index.js
@@ -17,7 +17,9 @@
 
 const functions = require('firebase-functions');
 const mkdirp = require('mkdirp-promise');
-const gcs = require('@google-cloud/storage')();
+// Include a Service Account Key to use a Signed URL
+const gcs = require('@google-cloud/storage')({keyFilename:'[InsertServiceAccountKey.json]'});
+const admin = require('firebase-admin');
 const spawn = require('child-process-promise').spawn;
 const LOCAL_TMP_FOLDER = '/tmp/';
 
@@ -26,13 +28,18 @@ const THUMB_MAX_HEIGHT = 200;
 const THUMB_MAX_WIDTH = 200;
 // Thumbnail prefix added to file names.
 const THUMB_PREFIX = 'thumb_';
+admin.initializeApp(functions.config().firebase);
+const ref = admin.database().ref();
 
 /**
  * When an image is uploaded in the Storage bucket We generate a thumbnail automatically using
  * ImageMagick.
  */
 exports.generateThumbnail = functions.storage.object().onChange(event => {
+  const fileBucket = event.data.bucket;
+  const bucket = gcs.bucket(fileBucket);
   const filePath = event.data.name;
+  const file = bucket.file(filePath);
   const filePathSplit = filePath.split('/');
   const fileName = filePathSplit.pop();
   const fileDir = filePathSplit.join('/') + (filePathSplit.length > 0 ? '/' : '');
@@ -40,6 +47,7 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
   const tempLocalDir = `${LOCAL_TMP_FOLDER}${fileDir}`;
   const tempLocalFile = `${tempLocalDir}${fileName}`;
   const tempLocalThumbFile = `${LOCAL_TMP_FOLDER}${thumbFilePath}`;
+  const thumbFile = bucket.file(thumbFilePath);
 
   // Exit if this is triggered on a file that is not an image.
   if (!event.data.contentType.startsWith('image/')) {
@@ -62,21 +70,40 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
   // Create the temp directory where the storage file will be downloaded.
   return mkdirp(tempLocalDir).then(() => {
     // Download file from bucket.
-    const bucket = gcs.bucket(event.data.bucket);
     return bucket.file(filePath).download({
       destination: tempLocalFile
-    }).then(() => {
-      console.log('The file has been downloaded to', tempLocalFile);
-      // Generate a thumbnail using ImageMagick.
-      return spawn('convert', [tempLocalFile, '-thumbnail', `${THUMB_MAX_WIDTH}x${THUMB_MAX_HEIGHT}>`, tempLocalThumbFile]).then(() => {
-        console.log('Thumbnail created at', tempLocalThumbFile);
-        // Uploading the Thumbnail.
-        return bucket.upload(tempLocalThumbFile, {
-          destination: thumbFilePath
-        }).then(() => {
-          console.log('Thumbnail uploaded to Storage at', thumbFilePath);
-        });
-      });
     });
+  }).then(() => {
+    console.log('The file has been downloaded to', tempLocalFile);
+    // Generate a thumbnail using ImageMagick.
+    return spawn('convert', [tempLocalFile, '-thumbnail', `${THUMB_MAX_WIDTH}x${THUMB_MAX_HEIGHT}>`, tempLocalThumbFile]);
+  }).then(() => {
+    console.log('Thumbnail created at', tempLocalThumbFile);
+    // Uploading the Thumbnail.
+    return bucket.upload(tempLocalThumbFile, {
+      destination: thumbFilePath
+    })
+  }).then(() => {
+    console.log('Thumbnail uploaded to Storage at', thumbFilePath);
+  }).then(() => {
+    const config = {
+      action: 'read',
+      expires: '03-01-2500'
+    };
+    // Get the Signed URL for the thumbnail and original images
+    return Promise.all([
+      thumbFile.getSignedUrl(config),
+      file.getSignedUrl(config),
+    ]);
+  }).then(results => {
+    console.log('Got Signed URL');
+    const thumbResult = results[0];
+    const originalResult = results[1];
+    const thumbFileUrl = thumbResult[0];
+    const fileUrl = originalResult[0];
+    // Add the URLs to the Database
+    return ref.child("images").push({path: fileUrl, thumbnail: thumbFileUrl});
+  }).catch(reason => {
+    console.log(reason);
   });
-});
+})
