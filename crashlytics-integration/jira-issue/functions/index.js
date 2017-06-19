beaea81a97c6efb3201ff297e5e4b1e69c00dd10
Xin Wei <xinwei@google.com>
2017-06-19 15:54:38 -0700

    add jira-issue-creator

---
+++ crashlytics-integration/jira-issue/functions/index.js
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+const functions = require('firebase-functions'),
+      rp = require('request-promise');
+
+const parseUrl = (url) => {
+    const matches = url.match(/(https?:\/\/)(.+?)(\/.+)?\/(projects|browse)\/([\w\-]+)/);
+    if (matches && matches.length === 6) {
+      return { protocol: matches[1], domain: matches[2], contextPath: matches[3] | '', projectKey: matches[5]}
+    } else {
+      throw new Error('Unexpected URL Format');
+    }
+};
+
+// Helper function that posts to Jira to create a new issue
+const createJiraIssue = (issueId, issueTitle) => {
+  const { projectUrl, user, pass, issueType, componentId } = functions.config().jira;
+  const { protocol, domain, contextPath, projectKey} = parseUrl(projectUrl);
+  const baseUrl = [protocol, domain, contextPath].join();
+  const url = `${baseUrl}/rest/api/2/issue`;
+
+  // see https://developer.atlassian.com/jiradev/jira-apis/jira-rest-apis/jira-rest-api-tutorials/jira-rest-api-example-create-issue
+  // to customize the new issue being created
+
+  const newIssue = {
+    "fields": {
+      "components": {
+        "id": componentId || '10000'
+      },
+      "project": { "id": projectKey },
+      "summary": `New Issue - ${issueId}`,
+      "description": issueTitle,
+      "issueType": {
+        "name": issueType || 'Bug'
+      }
+    }
+  };
+
+  // Uses Basic Authentication
+  // Jira Doc: https://developer.atlassian.com/jiradev/jira-apis/jira-rest-apis/jira-rest-api-tutorials/jira-rest-api-example-basic-authentication
+  // Request (Promise) Doc: https://github.com/request/request#http-authentication
+  return rp({
+    auth: {
+      'user': user,
+      'pass': pass
+    },
+    method: 'POST',
+    uri: url,
+    body: newIssue,
+    json: true
+  });
+};
+
+exports.createJiraOnIssue = functions.crashlytics.onNewIssue((event) => {
+  const { data } = event;
+  createJiraIssue(data.issueId, data.issueTitle).then(() => {
+    console.log(`Created issue ${data.issueId} successfully to Jira`);
+  }).catch(error => {
+    console.log('An error has occurred', error);
+  });
+});
