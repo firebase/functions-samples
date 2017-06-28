62f1e0b7417e4aef20f78ad4feeaeb77dadf8803
Nicolas Garnier <nivco@google.com>
2017-06-28 13:56:59 +0200

    Update quickstart/thumbnail sample to:  - Use os and path to construct file paths  - Delete files at the end of the function  - Make promises less deep
    Change-Id: Ifc597503cc0cb1ebde2d4f91b6e467c07b8dab50

--- quickstarts/thumbnails/functions/index.js
+++ quickstarts/thumbnails/functions/index.js
@@ -19,6 +19,9 @@
 const functions = require('firebase-functions');
 const gcs = require('@google-cloud/storage')();
 const spawn = require('child-process-promise').spawn;
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
 // [END import]
 
 // [START generateThumbnail]
@@ -47,7 +50,7 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
   }
 
   // Get the file name.
-  const fileName = filePath.split('/').pop();
+  const fileName = path.basename(filePath);
   // Exit if the image is already a thumbnail.
   if (fileName.startsWith('thumb_')) {
     console.log('Already a Thumbnail.');
@@ -59,7 +62,7 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
     console.log('This is a deletion event.');
     return;
   }
-  
+
   // Exit if file exists but is not new and is only being triggered
   // because of a metadata change.
   if (resourceState === 'exists' && metageneration > 1) {
@@ -71,22 +74,22 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
   // [START thumbnailGeneration]
   // Download file from bucket.
   const bucket = gcs.bucket(fileBucket);
-  const tempFilePath = `/tmp/${fileName}`;
+  const tempFilePath = path.join(os.tmpdir(), fileName);
   return bucket.file(filePath).download({
     destination: tempFilePath
   }).then(() => {
     console.log('Image downloaded locally to', tempFilePath);
     // Generate a thumbnail using ImageMagick.
-    return spawn('convert', [tempFilePath, '-thumbnail', '200x200>', tempFilePath]).then(() => {
-      console.log('Thumbnail created at', tempFilePath);
-      // We add a 'thumb_' prefix to thumbnails file name. That's where we'll upload the thumbnail.
-      const thumbFilePath = filePath.replace(/(\/)?([^\/]*)$/, '$1thumb_$2');
-      // Uploading the thumbnail.
-      return bucket.upload(tempFilePath, {
-        destination: thumbFilePath
-      });
-    });
-  });
+    return spawn('convert', [tempFilePath, '-thumbnail', '200x200>', tempFilePath]);
+  }).then(() => {
+    console.log('Thumbnail created at', tempFilePath);
+    // We add a 'thumb_' prefix to thumbnails file name. That's where we'll upload the thumbnail.
+    const thumbFileName = `thumb_${fileName}`;
+    const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
+    // Uploading the thumbnail.
+    return bucket.upload(tempFilePath, {destination: thumbFilePath});
+  // Once the thumbnail has been uploaded delete the local file to free up disk space.
+  }).then(() => fs.unlinkSync(tempFilePath));
   // [END thumbnailGeneration]
 });
 // [END generateThumbnail]
