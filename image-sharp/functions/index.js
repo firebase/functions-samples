2a557ed398ac1695a882ec2c525f548b4fa09da5
Nicolas Garnier <nivco.las@gmail.com>
2017-06-29 11:16:13 +0200

    Removed docs import tags

--- image-sharp/functions/index.js
+++ image-sharp/functions/index.js
@@ -15,24 +15,18 @@
  */
 'use strict';
 
-// [START import]
 const functions = require('firebase-functions');
 const gcs = require('@google-cloud/storage')();
 const sharp = require('sharp');
-// [END import]
 
 const THUMB_MAX_WIDTH = 200;
 const THUMB_MAX_HEIGHT = 200;
 
-// [START generateThumbnail]
 /**
  * When an image is uploaded in the Storage bucket We generate a thumbnail automatically using
  * ImageMagick.
  */
-// [START generateThumbnailTrigger]
 exports.generateThumbnail = functions.storage.object().onChange(event => {
-// [END generateThumbnailTrigger]
-  // [START eventAttributes]
   const object = event.data; // The Storage object.
 
   const fileBucket = object.bucket; // The Storage bucket that contains the file.
@@ -40,9 +34,7 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
   const contentType = object.contentType; // File content type.
   const resourceState = object.resourceState; // The resourceState is 'exists' or 'not_exists' (for file/folder deletions).
   const metageneration = object.metageneration; // Number of times metadata has been generated. New objects have a value of 1.
-  // [END eventAttributes]
 
-  // [START stopConditions]
   // Exit if this is triggered on a file that is not an image.
   if (!contentType.startsWith('image/')) {
     console.log('This is not an image.');
@@ -69,9 +61,7 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
     console.log('This is a metadata change event.');
     return;
   }
-  // [END stopConditions]
 
-  // [START thumbnailGeneration]
   // Download file from bucket.
   const bucket = gcs.bucket(fileBucket);
 
@@ -97,6 +87,4 @@ exports.generateThumbnail = functions.storage.object().onChange(event => {
   return streamAsPromise.then(() => {
     console.log('Thumbnail created successfully');
   });
-  // [END thumbnailGeneration]
 });
-// [END generateThumbnail]
