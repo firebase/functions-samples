133c421a166fc9e1c4457ab07c9390480a1f440b
Xin Wei <xinwei@google.com>
2017-07-18 15:57:02 -0700

    add regressedIssue and velocity alerts to samples

--- crashlytics-integration/slack-notifier/functions/index.js
+++ crashlytics-integration/slack-notifier/functions/index.js
@@ -19,23 +19,60 @@ const functions = require('firebase-functions'),
       rp = require('request-promise');
 
 // Helper function that posts to Slack about the new issue
-const notifySlack = (issueId, issueDesc) => {
+const notifySlack = slackMessage => {
   // see https://api.slack.com/docs/message-formatting on how
   // to customize the message payload
-  const slackMessage = {
-    text: `<!here|here> There's a new issue (${issueId}) in your app - ${issueDesc}`
-  };
   return rp({
     method: 'POST',
     uri: functions.config().slack.webhook_url,
-    body: slackMessage,
+    body: {
+      text: slackMessage,
+    },
     json: true
   });
 };
 
-exports.postSlackOnIssue = functions.crashlytics.onNewIssue(event => {
+exports.postOnNewIssue = functions.crashlytics.onNewIssue(event => {
   const { data } = event;
-  return notifySlack(data.issueId, data.issueTitle).then(() => {
-    console.log(`Posted issue ${data.issueId} successfully to Slack`);
+  // Available attributes for new issues
+  // data.issueId - {String} Issue id number
+  // data.issueTitle - {String} Issue Title (first line of the stack trace)
+  const { issueId, issueTitle } = data;
+  const slackMessage = `<!here|here> There's a new issue (${issueId}) ` +
+      `in your app - ${opts.issueTitle}`;
+  return notifySlack(slackMessage).then(() => {
+    console.log(`Posted new issue ${issueId} successfully to Slack`);
+  });
+});
+
+exports.postOnRegressedIssue = functions.crashlytics.onRegressedIssue(event => {
+  const { data } = event;
+  // Available attributes for regressed issues
+  // data.issueId - {String} Issue id number
+  // data.issueTitle - {String} Issue Title (first line of the stack trace)
+  // data.resolvedAt - {Long} Timestamp in which the issue was resolved at
+  const { issueId, issueTitle, resolvedAt } = data;
+  const slackMessage = `<!here|here> There's a regressed issue (${issueId}) ` +
+      `in your app - ${issueTitle}. This issue was previously resolved ` +
+      `at ${new Date(resolvedAt).toString()}`;
+  return notifySlack(slackMessage).then(() => {
+    console.log(`Posted regressed issue ${issueId} successfully to Slack`);
+  });
+});
+
+exports.postOnVelocityAlert = functions.crashlytics.onVelocityAlert(event => {
+  const { data } = event;
+  // Available attributes for regressed issues
+  // data.issueId - {String} Issue id number
+  // data.issueTitle - {String} Issue Title (first line of the stack trace)
+  // data.crashPercentage - {double} Crash Percentage. Total crashes divided by total # of sessions.
+  // data.buildVersion - {String} build version
+  // data.crashes - {double} # of Crashes
+  const { issueId, issueTitle, crashPercentage, buildVersion, crashes } = data;
+  const slackMessage = `<!here|here> There's an issue (${issueId}) on ` +
+      `version ${buildVersion} that is causing ` +
+      `${parseFloat(crashPercentage).toFixed(2)}% of all sessions to crash.`;
+  return notifySlack(slackMessage)/then(() => {
+    console.log(`Posted velocity alert ${issueId} successfully to Slack`);
   });
 });
