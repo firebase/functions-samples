b832b43342e5c4260e94860bcd6048e407e07e53
Deddy SAINT-VAL <kevded@hotmail.com>
2017-08-01 19:45:55 +0200

    Update

--- paypal/functions/index.js
+++ paypal/functions/index.js
@@ -1,4 +1,20 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 'use strict';
+
 const functions = require('firebase-functions');
 // CORS Express middleware to enable CORS Requests.
 const cors = require('cors')({origin: true});
@@ -12,10 +28,12 @@ paypal.configure({
     'client_id': functions.config().paypal.client_id, // run: firebase functions:config:set paypal.client_id="yourPaypalClientID" 
     'client_secret': functions.config().paypal.client_secret // run: firebase functions:config:set paypal.client_secret="yourPaypalClientSecret"
 });
-
+/**
+ * Expected in the body the amount
+ * Set up the payment information object
+ * Initialize the payment and redirect the user to the PayPal payment page
+ */
 exports.pay = functions.https.onRequest((req, res) => {
-    // Dev
-    cors(req, res, () => {});
     // 1.Set up a payment information object, Nuild PayPal payment request
     const payReq = JSON.stringify({
         intent: 'sale',
