d42b34ac3ba7c64cca726467a064502d9b1cc7d5
Abe Haskins <abeisgreat@abeisgreat.com>
2017-09-30 13:04:37 -0700

    Adds uppercase-firestore

---
+++ quickstarts/uppercase-firestore/functions/index.js
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+// [START all]
+// [START import]
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+// The Firebase Admin SDK to access the Firebase Realtime Database. 
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+// [END import]
+
+// [START addMessage]
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:documentId/original
+// [START addMessageTrigger]
+exports.addMessage = functions.https.onRequest((req, res) => {
+// [END addMessageTrigger]
+  // Grab the text parameter.
+  const original = req.query.text;
+  // [START adminSdkAdd]
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  admin.firestore().collection('messages').add({original: original}).then(writeResult => {
+    // Send back a message that we've succesfully written the message
+    res.json({result: `Message with ID: ${writeResult.id} added.`});
+  });
+  // [END adminSdkAdd]
+});
+// [END addMessage]
+
+// [START makeUppercase]
+// Listens for new messages added to /messages/:documentId/original and creates an
+// uppercase version of the message to /messages/:documentId/uppercase
+// [START makeUppercaseTrigger]
+exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
+    .onCreate(event => {
+// [END makeUppercaseTrigger]
+      // [START makeUppercaseBody]
+
+      // Grab the current value of what was written to the Realtime Database.
+      const original = event.data.data().original;
+      console.log('Uppercasing', event.params.documentId, original);
+      const uppercase = original.toUpperCase();
+      // You must return a Promise when performing asynchronous tasks inside a Functions such as
+      // writing to the Firebase Realtime Database.
+      // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+      return event.data.ref.set({uppercase}, {merge: true});
+      // [END makeUppercaseBody]
+    });
+// [END makeUppercase]
+// [END all]
