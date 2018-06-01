e16471cf6b599e87b37ad7522e4dd9ffe98f9c75
Nicolas Garnier <nivco.las@gmail.com>
2018-06-01 16:00:26 +0200

    resourceState does not exist anymore

--- ffmpeg-convert-audio/functions/index.js
+++ ffmpeg-convert-audio/functions/index.js
@@ -10,7 +10,7 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for t`he specific language governing permissions and
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 'use strict';
@@ -65,7 +65,6 @@ exports.generateMonoAudio = functions.storage.object().onFinalize((object) => {
   const fileBucket = object.bucket; // The Storage bucket that contains the file.
   const filePath = object.name; // File path in the bucket.
   const contentType = object.contentType; // File content type.
-  const resourceState = object.resourceState; // The resourceState is 'exists' or 'not_exists' (for file/folder deletions).
   const metageneration = object.metageneration; // Number of times metadata has been generated. New objects have a value of 1.
 
   // Exit if this is triggered on a file that is not an audio.
