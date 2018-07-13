04a8b480e6c0a2ec35c677648fde180940859dae
Nicolas Garnier <nivco.las@gmail.com>
2018-07-13 14:06:05 +0200

    Updated limit-children sample to ES2017 syntax and Node 8.

--- limit-children/functions/index.js
+++ limit-children/functions/index.js
@@ -22,20 +22,18 @@ const MAX_LOG_COUNT = 5;
 
 // Removes siblings of the node that element that triggered the function if there are more than MAX_LOG_COUNT.
 // In this example we'll keep the max number of chat message history to MAX_LOG_COUNT.
-exports.truncate = functions.database.ref('/chat/{messageid}').onWrite((change) => {
+exports.truncate = functions.database.ref('/chat/{messageid}').onWrite(async (change) => {
   const parentRef = change.after.ref.parent;
-  return parentRef.once('value').then((snapshot) => {
-    if (snapshot.numChildren() >= MAX_LOG_COUNT) {
-      let childCount = 0;
-      const updates = {};
-      snapshot.forEach((child) => {
-        if (++childCount <= snapshot.numChildren() - MAX_LOG_COUNT) {
-          updates[child.key] = null;
-        }
-      });
-      // Update the parent. This effectively removes the extra children.
-      return parentRef.update(updates);
-    }
-    return null;
-  });
+  const snapshot = await parentRef.once('value');
+  if (snapshot.numChildren() >= MAX_LOG_COUNT) {
+    let childCount = 0;
+    const updates = {};
+    snapshot.forEach((child) => {
+      if (++childCount <= snapshot.numChildren() - MAX_LOG_COUNT) {
+        updates[child.key] = null;
+      }
+    });
+    // Update the parent. This effectively removes the extra children.
+    return parentRef.update(updates);
+  }
 });
