3e5a96b0c9c67e4d9612757dd31465310b21a892
Nicolas Garnier <nivco.las@gmail.com>
2018-08-07 11:33:55 +0200

    Updating  bigquery-import to firebase-functions v1 syntax

--- bigquery-import/functions/index.js
+++ bigquery-import/functions/index.js
@@ -21,15 +21,15 @@ const bigquery = require('@google-cloud/bigquery')();
 /**
  * Writes all logs from the Realtime Database into bigquery.
  */
-exports.addtobigquery = functions.database.ref('/logs/{logid}').onWrite((event) => {
+exports.addtobigquery = functions.database.ref('/logs/{logid}').onCreate((snapshot) => {
   // TODO: Make sure you set the `bigquery.datasetName` Google Cloud environment variable.
   const dataset = bigquery.dataset(functions.config().bigquery.datasetname);
   // TODO: Make sure you set the `bigquery.tableName` Google Cloud environment variable.
   const table = dataset.table(functions.config().bigquery.tablename);
 
   return table.insert({
-    ID: event.data.key,
-    MESSAGE: event.data.val().message,
-    NUMBER: event.data.val().number,
+    ID: snapshot.key,
+    MESSAGE: snapshot.val().message,
+    NUMBER: snapshot.val().number,
   });
 });
