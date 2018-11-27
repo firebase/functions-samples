4e724222da73ec93b48607e72f36390a84fe8795
Nicolas Garnier <nivco.las@gmail.com>
2018-11-27 14:02:07 +0100

    Add import tags and comments everywhere

--- quickstarts/big-ben/functions/index.js
+++ quickstarts/big-ben/functions/index.js
@@ -15,18 +15,25 @@
  */
 'use strict';
 
+// [START import]
 const functions = require('firebase-functions');
 const express = require('express');
-const cors = require('cors')({origin: true});
 const app = express();
+// [END import]
 
+// [START middleware]
+const cors = require('cors')({origin: true});
 app.use(cors);
+// [END middleware]
 
+// [START index]
 // This endpoint provides displays the index page.
 app.get('/', (req, res) => {
   const date = new Date();
   const hours = (date.getHours() % 12) + 1; // London is UTC + 1hr;
+  // [START_EXCLUDE silent]
   res.set('Cache-Control', `public, max-age=${secondsLeftBeforeEndOfHour(date)}`);
+  // [END_EXCLUDE silent]
   res.send(`
   <!doctype html>
     <head>
@@ -40,21 +47,32 @@ app.get('/', (req, res) => {
     </body>
   </html>`);
 });
+// [END index]
 
+// [START api]
 // This endpoint is the BONG API.
 app.get('/api', (req, res) => {
   const date = new Date();
   const hours = (date.getHours() % 12) + 1; // London is UTC + 1hr;
+  // [START_EXCLUDE silent]
+  // [START cache]
   res.set('Cache-Control', `public, max-age=${secondsLeftBeforeEndOfHour(date)}`);
+  // [END cache]
+  // [END_EXCLUDE silent]
   res.send('BONG '.repeat(hours));
 });
+// [END api]
 
+// [START seconds_left]
 // Returns the number of seconds left before the next hour starts.
 function secondsLeftBeforeEndOfHour(date) {
   const m = date.getMinutes();
   const s = date.getSeconds();
   return 3600 - (m*60) - s;
 }
+// [END seconds_left]
 
-// 
+// [START export]
+// Export the express app as an HTTP Cloud Functions
 exports.app = functions.https.onRequest(app);
+// [END export]
