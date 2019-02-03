044ba742ea74568b8146c13f9336bfbe1c71915a
Rishitosh Kumar Singh <30952286+rishitoshsingh@users.noreply.github.com>
2019-02-03 16:43:11 +0530

    Update index.js

--- limit-children/functions/index.js
+++ limit-children/functions/index.js
@@ -22,14 +22,15 @@ const MAX_LOG_COUNT = 5;
 
 // Removes siblings of the node that element that triggered the function if there are more than MAX_LOG_COUNT.
 // In this example we'll keep the max number of chat message history to MAX_LOG_COUNT.
-exports.truncate = functions.database.ref('/chat/{messageid}').onWrite(async (change) => {
-  const parentRef = change.after.ref.parent;
-  const snapshot = await parentRef.once('value');
-  if (snapshot.numChildren() >= MAX_LOG_COUNT) {
+exports.truncate = functions.database.ref('/chat').onWrite((change) => {
+  const parentRef = change.after.ref;
+  const snapshot = change.after
+
+  if (snapshot.numChildren() >= MAX_USERS) {
     let childCount = 0;
     const updates = {};
     snapshot.forEach((child) => {
-      if (++childCount <= snapshot.numChildren() - MAX_LOG_COUNT) {
+      if (++childCount <= snapshot.numChildren() - MAX_USERS) {
         updates[child.key] = null;
       }
     });
