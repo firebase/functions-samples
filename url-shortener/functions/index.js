2e1340691762d1d5c1cc298efe5d9c7c96557720
Sam <samstern@google.com>
2020-12-10 11:37:35 +0000

    remote-config-diff, spotify-auth, stripe, survey-app-update, template-handlebars, testlab-to-slack, text-moderation, typescript-getting-started, url-shortener, username-password-auth, vision-annotate-images

--- url-shortener/functions/index.js
+++ url-shortener/functions/index.js
@@ -16,16 +16,19 @@
 'use strict';
 
 const functions = require('firebase-functions');
-const BitlyClient = require('bitly');
+const { BitlyClient } = require('bitly');
 // TODO: Make sure to set the bitly.access_token cloud functions config using the CLI.
-const bitly = BitlyClient(functions.config().bitly.access_token);
+const bitly = new BitlyClient(functions.config().bitly.access_token);
 
 // Shorten URL written to /links/{linkID}.
 exports.shortenUrl = functions.database.ref('/links/{linkID}').onCreate(async (snap) => {
   const originalUrl = snap.val();
   const response = await bitly.shorten(originalUrl);
+  // @ts-ignore
+  const shortUrl = response.url;
+
   return snap.ref.set({
     original: originalUrl,
-    short: response.data.url,
+    short: shortUrl,
   })
 });
