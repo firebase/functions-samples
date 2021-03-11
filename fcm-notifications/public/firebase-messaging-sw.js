2e17f96daeedb3ae985404a3a4e21d67428a2175
DPEBot <dpebot@google.com>
2021-03-11 02:46:26 -0800

    Auto-update dependencies. (#838)

--- fcm-notifications/public/firebase-messaging-sw.js
+++ fcm-notifications/public/firebase-messaging-sw.js
@@ -1,8 +1,8 @@
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('/__/firebase/8.2.10/firebase-app.js');
-importScripts('/__/firebase/8.2.10/firebase-messaging.js');
+importScripts('/__/firebase/8.3.0/firebase-app.js');
+importScripts('/__/firebase/8.3.0/firebase-messaging.js');
 importScripts('/__/firebase/init.js');
 
 firebase.messaging();
