b825b1c9f9552194e2c2e781eb9c4ac7fe4c55b2
jhuleatt <jhuleatt@google.com>
2021-05-10 13:52:17 -0400

    make linter happy

--- quickstarts/runtime-options/functions/index.js
+++ quickstarts/runtime-options/functions/index.js
@@ -1,42 +1,42 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
 // [START runtimeMinInstances]
 exports.getAutocompleteResponse = functions
-  .runWith(
+    .runWith(
     // Keep 50 instances hot for this latency-critical function
-    {
-      minInstances: 50,
-    }
-  )
-  .https.onCall((data, context) => {
+        {
+          minInstances: 50,
+        }
+    )
+    .https.onCall((data, context) => {
     // Autocomplete a user's search term
-  });
+    });
 // [END runtimeMinInstances]
 
 // [START runtimeMaxInstances]
 exports.mirrorOrdersToLegacyDatabase = functions
-  .runWith(
+    .runWith(
     // Legacy database only supports 100 simultaneous connections
-    { maxInstances: 100 }
-  )
-  .firestore.document('orders/{orderId}')
-  .onWrite((change, context) => {
+        {maxInstances: 100}
+    )
+    .firestore.document("orders/{orderId}")
+    .onWrite((change, context) => {
     // Connect to legacy database
-  });
+    });
 // [END runtimeMaxInstances]
 
 // [START runtimeTimeoutMemory]
 exports.convertLargeFile = functions
-  .runWith(
+    .runWith(
     // These options ensure the function has enough memory and time
     // to process large files
-    {
-      timeoutSeconds: 300,
-      memory: '1GB',
-    }
-  )
-  .storage.object()
-  .onFinalize((object) => {
+        {
+          timeoutSeconds: 300,
+          memory: "1GB",
+        }
+    )
+    .storage.object()
+    .onFinalize((object) => {
     // Do some complicated things that take a lot of memory and time
-  });
+    });
 // [END runtimeTimeoutMemory]
