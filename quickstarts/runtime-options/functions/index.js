28971c6ae0772761ec3a4a17f33d58ddecb31c02
Jeff <jhuleatt@google.com>
2021-05-10 14:24:41 -0400

    Add runtime options sample (#862)

---
+++ quickstarts/runtime-options/functions/index.js
@@ -0,0 +1,44 @@
+const functions = require("firebase-functions");
+
+// [START runtimeMinInstances]
+exports.getAutocompleteResponse = functions
+    .runWith(
+        // Keep 5 instances warm for this latency-critical function
+        {
+          minInstances: 5,
+        }
+    )
+    .https.onCall((data, context) => {
+      // Autocomplete a user's search term
+    });
+// [END runtimeMinInstances]
+
+// [START runtimeMaxInstances]
+exports.mirrorOrdersToLegacyDatabase = functions
+    .runWith(
+        // Legacy database only supports 100 simultaneous connections
+        {
+          maxInstances: 100,
+        }
+    )
+    .firestore.document("orders/{orderId}")
+    .onWrite((change, context) => {
+      // Connect to legacy database
+    });
+// [END runtimeMaxInstances]
+
+// [START runtimeTimeoutMemory]
+exports.convertLargeFile = functions
+    .runWith(
+        // Ensure the function has enough memory and time
+        // to process large files
+        {
+          timeoutSeconds: 300,
+          memory: "1GB",
+        }
+    )
+    .storage.object()
+    .onFinalize((object) => {
+      // Do some complicated things that take a lot of memory and time
+    });
+// [END runtimeTimeoutMemory]
