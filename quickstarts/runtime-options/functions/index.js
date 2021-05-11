7f520c64f6cda7d30c1932b9cf7e06d82af05399
jhuleatt <jhuleatt@google.com>
2021-05-11 09:51:16 -0400

    more compact samples

--- quickstarts/runtime-options/functions/index.js
+++ quickstarts/runtime-options/functions/index.js
@@ -2,25 +2,22 @@ const functions = require("firebase-functions");
 
 // [START runtimeMinInstances]
 exports.getAutocompleteResponse = functions
-    .runWith(
-        // Keep 5 instances warm for this latency-critical function
-        {
-          minInstances: 5,
-        }
-    )
+    .runWith({
+      // Keep 5 instances warm for this latency-critical function
+      minInstances: 5,
+    })
     .https.onCall((data, context) => {
       // Autocomplete a user's search term
     });
 // [END runtimeMinInstances]
 
+
 // [START runtimeMaxInstances]
 exports.mirrorOrdersToLegacyDatabase = functions
-    .runWith(
-        // Legacy database only supports 100 simultaneous connections
-        {
-          maxInstances: 100,
-        }
-    )
+    .runWith({
+      // Legacy database only supports 100 simultaneous connections
+      maxInstances: 100,
+    })
     .firestore.document("orders/{orderId}")
     .onWrite((change, context) => {
       // Connect to legacy database
@@ -29,14 +26,12 @@ exports.mirrorOrdersToLegacyDatabase = functions
 
 // [START runtimeTimeoutMemory]
 exports.convertLargeFile = functions
-    .runWith(
-        // Ensure the function has enough memory and time
-        // to process large files
-        {
-          timeoutSeconds: 300,
-          memory: "1GB",
-        }
-    )
+    .runWith({
+      // Ensure the function has enough memory and time
+      // to process large files
+      timeoutSeconds: 300,
+      memory: "1GB",
+    })
     .storage.object()
     .onFinalize((object) => {
       // Do some complicated things that take a lot of memory and time
