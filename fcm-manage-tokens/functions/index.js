951c36d10e1363de5bba7164d865bcf3af1ca79d
Arthur Thompson <arthur.thompson.jr@gmail.com>
2022-02-19 16:41:29 -0800

    update pruneTokens comment

--- fcm-manage-tokens/functions/index.js
+++ fcm-manage-tokens/functions/index.js
@@ -40,6 +40,9 @@ exports.updateToken = functions.https.onCall(async (data, context) => {
 /**
  * Scheduled function that runs once a day. It retrieves all stale tokens then
  * unsubscribes them from 'topic1' then deletes them.
+ *
+ * Note: topic1 is an example topic here. It is up to the developer to unsubscribe
+ * all topics the token is subscribed to.
  */
 exports.pruneTokens = functions.pubsub.schedule('every 24 hours').onRun(async (context) => {
   const staleTokensResult = await admin.firestore().collection('tokens')
