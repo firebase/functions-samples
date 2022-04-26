606d05256348ebff66e3a366e0970edf68b3a9a1
rosariopf <rosariopf@google.com>
2022-04-26 11:26:30 +0100

    create hello pub sub function

---
+++ 2nd-gen/pubsub-helloworld/functions/index.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2022 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+"use strict";
+
+// [START import]
+const {onMessagePublished} = require("firebase-functions/v2/pubsub");
+const logger = require("firebase-functions/logger");
+// [END import]
+
+// [START helloWorld]
+/**
+ * Cloud Function to be triggered by Pub/Sub that logs a message using the
+ *  data published to the topic.
+ */
+// [START trigger]
+exports.hellopubsub = onMessagePublished("topic-name", (event) => {
+  // [END trigger]
+  // [START readBase64]
+  // Decode the PubSub Message body.
+  const message = event.data.message;
+  const messageBody = message.data ?
+    Buffer.from(message.data, "base64").toString() :
+    null;
+  // [END readBase64]
+  // Print the message in the logs.
+  logger.log(`Hello ${messageBody || "World" }!`);
+  return null;
+});
+// [END helloWorld]
