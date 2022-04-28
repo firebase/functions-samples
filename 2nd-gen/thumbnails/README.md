f735a8aaaeb4e360dcfde6cba655e4cb8898361c
Rachel Myers <rachelmyers@google.com>
2022-04-28 12:29:40 -0700

    Porting Storage trigger sample

---
+++ 2nd-gen/thumbnails/README.md
@@ -0,0 +1,43 @@
+# Firebase SDK for Cloud Functions Quickstart - Cloud Storage trigger
+
+This quickstart demonstrates using **Firebase SDK for Cloud Functions** setup with a Cloud Storage trigger.
+
+## Introduction
+
+This sample automatically generates thumbnails for images that are uploaded to Cloud Storage.
+
+
+## Functions Code
+
+See file [functions/index.js](functions/index.js) for the thumbnail generation code.
+
+The thumbnail generation is performed using ImageMagick which is installed by default on all Cloud Functions instances. This is a CLI so we execute the command from node using the [child-process-promise](https://www.npmjs.com/package/child-process-promise) package. The image is first downloaded locally from the Cloud Storage bucket to the `tmp` folder using the [google-cloud](https://github.com/GoogleCloudPlatform/google-cloud-node) SDK.
+
+The dependencies are listed in [functions/package.json](functions/package.json).
+
+
+## Trigger rules
+
+The function triggers on upload of any file to your Firebase project's default Cloud Storage bucket.
+
+
+## Deploy and test
+
+To deploy and test the sample:
+
+ - Create a Firebase project on the [Firebase Console](https://console.firebase.google.com) and visit the **Storage** tab.
+ - Get the code, for instance using `git clone https://github.com/firebase/functions-samples`
+ - Enter the correct directory `cd functions-samples/quickstarts/thumbnails`
+ - Setup the CLI to use your Firebase project using `firebase use --add` and select your Firebase project
+ - Deploy your project's code using `firebase deploy`
+ - Go to the Firebase Console **Storage** tab and upload an image. After a short time a thumbnail image with the same name but a `thumb_` prefix will be created in the same folder (make sure you refresh the UI to see the new file).
+
+
+## Contributing
+
+We'd love that you contribute to the project. Before doing so please read our [Contributor guide](../../CONTRIBUTING.md).
+
+
+## License
+
+© Google, 2022. Licensed under an [Apache-2](../../LICENSE) license.
---
+++ 2nd-gen/thumbnails/functions/index.js
@@ -0,0 +1,88 @@
+/**
+ * Copyright 2022 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+// [START v2storageImports]
+// [START v2storageSDKImport]
+const { onFinalize } = require('firebase-functions/v2/storage');
+// [END v2storageSDKImport]
+
+// [START v2storageAdditionalImports]
+const { initializeApp, storage} = require ('firebase-admin/app');
+const logger = require('firebase-functions/logger');
+const spawn = require('child-process-promise').spawn;
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+
+initializeApp()
+// [END v2storageAdditionalImports]
+// [END v2storageImports]
+
+// [START v2storageGenerateThumbnail]
+/**
+ * When an image is uploaded in the Storage bucket We generate a thumbnail automatically using
+ * ImageMagick.
+ */
+// [START v2storageGenerateThumbnailTrigger]
+exports.generateThumbnail = onFinalize(async (object) => {
+// [END v2storageGenerateThumbnailTrigger]
+
+  // [START v2storageEventAttributes]
+  const fileBucket = object.bucket; // The Storage bucket containing the file.
+  const filePath = object.name; // File path in the bucket.
+  const contentType = object.contentType; // File content type.
+  // [END v2storageEventAttributes]
+
+  // [START v2storageStopConditions]
+  // Exit if this is triggered on a file that is not an image.
+  if (!contentType.startsWith('image/')) {
+    return logger.log('This is not an image.');
+  }
+  // Exit if the image is already a thumbnail.
+  const fileName = path.basename(filePath);
+  if (fileName.startsWith('thumb_')) {
+    return logger.log('Already a Thumbnail.');
+  }
+  // [END v2storageStopConditions]
+
+  // [START v2storageThumbnailGeneration]
+  // Download file from bucket.
+  const bucket = storage().bucket(fileBucket);
+  const tempFilePath = path.join(os.tmpdir(), fileName);
+  const metadata = { contentType: contentType };
+
+  await bucket.file(filePath).download({destination: tempFilePath});
+  logger.log('Image downloaded locally to', tempFilePath);
+  // Generate a thumbnail using ImageMagick.
+  await spawn('convert', [tempFilePath, '-thumbnail', '200x200>', tempFilePath]);
+  logger.log('Thumbnail created at', tempFilePath);
+  
+  // Prefix 'thumb_' to file name.
+  const thumbFileName = `thumb_${fileName}`;
+  const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
+  
+  // Uploading the thumbnail.
+  await bucket.upload(tempFilePath, {
+    destination: thumbFilePath,
+    metadata: metadata,
+  });
+  
+  // Once the thumbnail has been uploaded delete the local file to free up disk space.
+  return fs.unlinkSync(tempFilePath);
+  // [END v2storageThumbnailGeneration]
+});
+// [END v2storageGenerateThumbnail]
