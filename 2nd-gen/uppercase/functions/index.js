ab07ed77df1bc86f0c7d1a2c3824b2b561a17e51
rosariopf <rosariopf@google.com>
2022-06-20 09:34:14 +0100

    init functions

---
+++ 2nd-gen/uppercase/functions/index.js
@@ -0,0 +1,9 @@
+const functions = require("firebase-functions");
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
