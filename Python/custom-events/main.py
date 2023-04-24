0f1ce53454895a6b7881e9af7f0e13949495bcf2
Kevin Cheung <kevincheung@google.com>
2023-04-24 13:53:19 -0700

    Non-default channel

--- Python/custom-events/main.py
+++ Python/custom-events/main.py
@@ -34,3 +34,20 @@ def onimageresized(event: eventarc_fn.CloudEvent) -> None:
     doc = collection.document(event.subject.replace("/", "_"))  # original file path
     doc.set(event.data)  # resized images paths and sizes
 # [END imageresizedEvent]
+
+
+
+# [START nondefaultchannel]
+@eventarc_fn.on_custom_event_published(
+    event_type="firebase.extensions.storage-resize-images.v1.complete",
+    channel="locations/us-west1/channels/firebase",
+    region="us-west1")
+def onimageresizedwest(event: eventarc_fn.CloudEvent) -> None:
+    print("Received image resize completed event: ", event.type)
+
+    # For example, write resized image details into Firestore.
+    firestore_client: google.cloud.firestore.Client = firestore.client()
+    collection = firestore_client.collection("images")
+    doc = collection.document(event.subject.replace("/", "_"))  # original file path
+    doc.set(event.data)  # resized images paths and sizes
+# [END nondefaultchannel]
