e5593ce286e32318dd0133805e13eff06b8393e9
Kevin Cheung <kevinthecheung@users.noreply.github.com>
2023-10-26 11:33:52 -0700

    FCM sample updated for CF3v2 (#1112)
    * FCM sample updated for CF3v2
    
    * pnpm lock file

---
+++ Node/fcm-notifications/functions/.eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    es2022: true,
+    node: true,
+  },
+  parserOptions: {
+    "ecmaVersion": 2022,
+    "sourceType": "module",
+  },
+  extends: [
+    "eslint:recommended",
+    "google",
+  ],
+  rules: {
+    "no-restricted-globals": ["error", "name", "length"],
+    "prefer-arrow-callback": "error",
+    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+  },
+  overrides: [
+    {
+      files: ["**/*.spec.*"],
+      env: {
+        mocha: true,
+      },
+      rules: {},
+    },
+  ],
+  globals: {},
+};
