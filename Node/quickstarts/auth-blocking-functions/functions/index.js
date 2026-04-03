4ffecf370bdf4f43db33943f5a7cc25770bc5f45
macastelaz <34776182+macastelaz@users.noreply.github.com>
2026-04-03 13:07:16 -0500

    Fix lint issues (lines too long, missing require)
    Refactor error handling to improve readability by formatting error messages across multiple lines.

--- Node/quickstarts/auth-blocking-functions/functions/index.js
+++ Node/quickstarts/auth-blocking-functions/functions/index.js
@@ -21,6 +21,7 @@ const {
   beforeSmsSent,
   HttpsError,
 } = require("firebase-functions/identity");
+const {defineString} = require('firebase-functions/params');
 const admin = require("firebase-admin");
 
 admin.initializeApp();
@@ -85,10 +86,13 @@ exports.checkEmailDomain = beforeEmailSent((event) => {
   // Only users of a specific domain can receive emails.
   if (!email) {
     // Throw an HttpsError so that Firebase Auth rejects the email sending.
-    throw new HttpsError("invalid-argument", "No email was found in the CloudEvent");
+    throw new HttpsError("invalid-argument",
+                         "No email was found in the CloudEvent");
   }
   if (!email.endsWith("@acme.com")) {
-    throw new HttpsError("permission-denied", "Only users from the acme.com domain can authenticate");
+    throw new HttpsError("permission-denied", 
+                         "Only users from the acme.com domain can " + 
+                         "authenticate");
   }
   // [END v2emailHttpsError]
 });
@@ -113,9 +117,10 @@ exports.checkPhoneNumber = beforeSmsSent((event) => {
   // [START v2smsHttpsError]
   if (!phoneNumber) {
     // Throw an HttpsError so that Firebase Auth rejects the SMS sending.
-    throw new HttpsError("invalid-argument", "No phone number was found in the CloudEvent");
+    throw new HttpsError("invalid-argument",
+                         "No phone number was found in the CloudEvent");
   }
-  
+
   // Only users of a specific region can receive SMS.
   if (!phoneNumber.startsWith(intlPrefixNumber.value())) {
     // Throw an HttpsError so that Firebase Auth rejects the SMS sending.
